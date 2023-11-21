dc4a68295652ec9383852f36b5717a4a9370d023
Paolo Sciarra <itbhps@gmail.com>
2023-11-21 08:17:49 +0100

    now we can calculate next-generation

--- src/main/java/it/twinsbrain/dojo/gameoflife/GameOfLife.java
+++ src/main/java/it/twinsbrain/dojo/gameoflife/GameOfLife.java
@@ -2,6 +2,7 @@ package it.twinsbrain.dojo.gameoflife;
 
 public class GameOfLife {
 
+    public static final char ALIVE = '*';
     private final char[][] grid;
 
     private GameOfLife(String representation) {
@@ -24,6 +25,50 @@ public class GameOfLife {
 
     public boolean isAliveCellAt(X x, Y y) {
         return grid[y.value][x.value] == '*';
+    }
+
+    public String nextGeneration() {
+        int columns = grid[0].length;
+        var nexGrid = new char[grid.length][columns];
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < columns; x++) {
+                nexGrid[y][x] = shouldBeAlive(y, x) ? '*' : '.';
+            }
+        }
+        var nextRepresentation = new StringBuilder();
+        for (int i = 0; i < grid.length; i++) {
+            nextRepresentation.append(new String(nexGrid[nexGrid.length - i - 1]));
+            if (i < grid.length - 1) {
+                nextRepresentation.append("\n");
+            }
+        }
+        return nextRepresentation.toString();
+    }
+
+    private boolean shouldBeAlive(int y, int x) {
+        int aliveNeighBoors = aliveNeighBoors(y, x);
+        if (grid[y][x] == ALIVE) {
+            return aliveNeighBoors >= 2 && aliveNeighBoors <= 3;
+        } else {
+            return aliveNeighBoors == 3;
+        }
+    }
+
+    private int aliveNeighBoors(int j, int i) {
+        var count = 0;
+        var minY = Math.max(0, j - 1);
+        var maxY = Math.min(j + 1, grid.length - 1);
+        var minX = Math.max(0, i - 1);
+        var maxX = Math.min(i + 1, grid[0].length - 1);
+        for (int y = minY; y <= maxY; y++) {
+            for (int x = minX; x <= maxX; x++) {
+                boolean current = x == i && y == j;
+                if (!current) {
+                    count += grid[y][x] == ALIVE ? 1 : 0;
+                }
+            }
+        }
+        return count;
     }
 
     public record X(int value) {
--- src/test/java/it/twinsbrain/dojo/gameoflife/GameOfLifeTest.java
+++ src/test/java/it/twinsbrain/dojo/gameoflife/GameOfLifeTest.java
@@ -4,6 +4,8 @@ import it.twinsbrain.dojo.gameoflife.GameOfLife.X;
 import it.twinsbrain.dojo.gameoflife.GameOfLife.Y;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -45,4 +47,49 @@ class GameOfLifeTest {
         }
     }
 
+    @Nested
+    class NextGeneration {
+
+        @ParameterizedTest(name = "{0} becomes {1}")
+        @CsvSource({
+                "., .",
+                "*, .",
+                ".***., ..*..",
+                "..**.., ......"
+        })
+        void up_to_unidimensional_next_generation_works(String start, String expectedNext) {
+            var gameOfLife = GameOfLife.from(start);
+            assertThat(gameOfLife.nextGeneration(), is(expectedNext));
+        }
+
+        @Test
+        void two_dimensional_should_work() {
+            var gameOfLife = GameOfLife.from(
+                    "..**..\n" +
+                            "..*..."
+            );
+            assertThat(gameOfLife.nextGeneration(), is(
+                    "..**..\n" +
+                            "..**.."
+            ));
+        }
+
+        @Test
+        void acceptance_test() {
+            var gameOfLife = GameOfLife.from(
+                    """
+                            ........
+                            ....*...
+                            ...**...
+                            ........"""
+            );
+            assertThat(gameOfLife.nextGeneration(), is(
+                    """
+                            ........
+                            ...**...
+                            ...**...
+                            ........"""
+            ));
+        }
+    }
 }
